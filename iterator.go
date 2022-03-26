bb6e2a7e5e1c896bd6d93c8bc0019730ee3ab6d6
Felix Kästner <mail@felix-kaestner.com>
2022-03-26 13:02:56 +0100

    add indexed iterator

--- iterator.go
+++ iterator.go
@@ -26,15 +26,20 @@ type (
 		HasNext() bool
 	}
 
-	// sliceIterator is the struct type that implements the
-	// Iterator interface for slices.
-	sliceIterator[T any] struct {
+	// IndexedValue represents a value from a collection or sequence,
+	// along with its associated index in that collection or sequence.
+	IndexedValue[T any] struct {
 		index int
-		slice []T
+		value T
 	}
 )
 
-// Next returns the next item in the iteration.
+// sliceIterator implements the Iterator interface for slices.
+type sliceIterator[T any] struct {
+	index int
+	slice []T
+}
+
 func (s *sliceIterator[T]) Next() (*T, error) {
 	if s.index >= len(s.slice) {
 		return nil, Done
@@ -44,7 +49,6 @@ func (s *sliceIterator[T]) Next() (*T, error) {
 	return &item, nil
 }
 
-// HasNext returns true if the iteration has more elements.
 func (s *sliceIterator[T]) HasNext() bool {
 	return s.index < len(s.slice)
 }
@@ -54,5 +58,41 @@ func FromSlice[T any](slice []T) Iterator[T] {
 	return &sliceIterator[T]{
 		index: 0,
 		slice: slice,
+	}
+}
+
+// indexedIterator implements the Iterator interface for
+// indexed collections.
+// It wraps an existing Iterator and each element produced
+// by the original iterator into an IndexedValue containing
+// the index of that element and the element itself.
+type indexedIterator[T any] struct {
+	index int
+	iter  Iterator[T]
+}
+
+func (i *indexedIterator[T]) Next() (*IndexedValue[T], error) {
+	item, err := i.iter.Next()
+	if err != nil {
+		return nil, err
+	}
+	i.index++
+	return &IndexedValue[T]{
+		index: i.index,
+		value: *item,
+	}, nil
+}
+
+func (i *indexedIterator[T]) HasNext() bool {
+	return i.iter.HasNext()
+}
+
+// WithIndex returns a new Iterator that wraps each element
+// produced by the original iterator into an IndexedValue
+// containing the index of that element and the element itself.
+func WithIndex[T any](iter Iterator[T]) Iterator[IndexedValue[T]] {
+	return &indexedIterator[T]{
+		index: -1,
+		iter:  iter,
 	}
 }
--- iterator_test.go
+++ iterator_test.go
@@ -58,15 +58,59 @@ func TestSliceIterator(t *testing.T) {
 
 		assertEqual(t, i.HasNext(), true)
 		item, _ := i.Next()
-		assertEqual(t, *item, 1)
+		assertEqual(t, 1, *item)
 
 		assertEqual(t, i.HasNext(), true)
 		item, _ = i.Next()
-		assertEqual(t, *item, 2)
+		assertEqual(t, 2, *item)
 
 		assertEqual(t, i.HasNext(), true)
 		item, _ = i.Next()
-		assertEqual(t, *item, 3)
+		assertEqual(t, 3, *item)
+
+		assertEqual(t, i.HasNext(), false)
+		_, err := i.Next()
+		assertEqual(t, Done, err)
+	}
+}
+
+func TestIndexedIterator(t *testing.T) {
+	{
+		s := []int{1, 2, 3}
+		i := WithIndex(FromSlice(s))
+
+		c := 0
+		for i.HasNext() {
+			c++
+			item, _ := i.Next()
+			assertEqual(t, c, item.value)
+			assertEqual(t, c-1, item.index)
+		}
+
+		assertEqual(t, i.HasNext(), false)
+
+		_, err := i.Next()
+		assertEqual(t, Done, err)
+	}
+
+	{
+		s := []int{1, 2, 3}
+		i := WithIndex(FromSlice(s))
+
+		assertEqual(t, i.HasNext(), true)
+		item, _ := i.Next()
+		assertEqual(t, 1, item.value)
+		assertEqual(t, 0, item.index)
+
+		assertEqual(t, i.HasNext(), true)
+		item, _ = i.Next()
+		assertEqual(t, 2, item.value)
+		assertEqual(t, 1, item.index)
+
+		assertEqual(t, i.HasNext(), true)
+		item, _ = i.Next()
+		assertEqual(t, 3, item.value)
+		assertEqual(t, 2, item.index)
 
 		assertEqual(t, i.HasNext(), false)
 		_, err := i.Next()
